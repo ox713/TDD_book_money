6d607af20b3610bdfe7087d735c4d878ffd38fd8
ox713 <60876711+ox713@users.noreply.github.com>
2020-05-12 15:40:00 +0400

    4v ввели паттерн объект значение, по итого котороо пришлось вводить функцию провертки равенства объектв equality

--- tests/test_tdd.py
+++ tests/test_tdd.py
@@ -14,7 +14,9 @@ class TestTdd(unittest.TestCase):
         product = five.times(3)
         self.assertEqual(15, product.amount)
 
-
+    # использование паттерна объект-значение для тогочтобы 5 д были 5 до
+    def test_equality(self):
+        self.assertTrue(Dollar(5).equals(Dollar(5)))
 
 if __name__ == "__main__":
     unittest.main()
