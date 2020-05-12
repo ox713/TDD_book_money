f28d3b88702da6f6c142f83aee256964b6559117
ox713 <60876711+ox713@users.noreply.github.com>
2020-05-12 14:43:59 +0400

    2 при нескольких тестах объект изменяеться. Создали доп.переменную

--- tests/test_tdd.py
+++ tests/test_tdd.py
@@ -7,10 +7,13 @@ from TDD import *
 
 class TestTdd(unittest.TestCase):
     def test_mul(self):
-        # дублирование 5*2
+        # объект не должен меняться при взаимодействии
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product)
+        product = five.times(3)
+        self.assertEqual(15, product)
+
 
 
 if __name__ == "__main__":
